d3bd1496c99ef2f3c41d5dc6fb1fe79c1488c6f4
Andika Rafa Akbar <andikarafa1221@gmail.com>
2026-06-14 18:40:15 +0700

    feat(api-gateway): add request logger middleware

--- express-gateway/src/index.js
+++ express-gateway/src/index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const requestLogger = require("./middleware/logger");
+
 const app = express();
 const port = process.env.PORT || 3060;
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.get("/", (req, res) => {
   res.json({
