31def7bd1b373d5b2fa946516cf1f39206600e01
vancebol <arkhandika.bw@gmail.com>
2026-06-20 20:54:32 +0700

    feat (citizen-service): create reportValidator to validate report by citizens

---
+++ php-citizen/app/validators/reportValidator.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\validators;
+
+class reportValidator {
+    public static function validate(array $data): array {
+        if (empty($data['category']) || empty($data['description'])) {
+            throw new \Exception("Kategori dan deskripsi laporan wajib diisi.");
+        }
+        return $data;
+    }
+}
